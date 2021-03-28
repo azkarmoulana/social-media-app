db929c5d87044e211e186b39007f33ac6cd32db9
azkarmoulana <azkar.moulana@gmail.com>
2021-03-28 22:11:09 +0530

    post operation mutations

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,14 +1,87 @@
+const { AuthenticationError, UserInputError } = require("apollo-server");
+const { PossibleTypeExtensionsRule } = require("graphql");
+
 const Post = require("../../models/Post");
+const checkAuth = require("../../utils/auth");
 
 module.exports = {
   Query: {
     getPosts: async () => {
       try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
       } catch (e) {
         throw new Error(e);
       }
+    },
+    getPost: async (_, { postId }) => {
+      try {
+        const post = await Post.findById(postId);
+
+        if (post) {
+          return post;
+        } else {
+          throw new Error("Post not found");
+        }
+      } catch (e) {
+        throw new Error(e);
+      }
+    },
+  },
+  Mutation: {
+    createPost: async (_, { body }, context) => {
+      const user = checkAuth(context);
+
+      const newPost = new Post({
+        body,
+        user: user.id,
+        username: user.username,
+        createdAt: new Date().toISOString(),
+      });
+
+      const post = newPost.save();
+
+      return post;
+    },
+    deletePost: async (_, { postId }, context) => {
+      const user = checkAuth(context);
+
+      try {
+        const post = await Post.findById(postId);
+
+        if (user.username === post.username) {
+          await post.delete();
+          return "Successfully deleted the post";
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
+      } catch (e) {
+        throw new Error(e);
+      }
+    },
+    createComment: async (_, { postId, body }, context) => {
+      const { username } = checkAuth(context);
+
+      if (body.trim() === "") {
+        throw new UserInputError("Empty comment", {
+          errors: {
+            body: "Comments body must not be empty",
+          },
+        });
+      }
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+        post.comments.unshift({
+          body,
+          username,
+          createdAt: new Date().toISOString(),
+        });
+
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post not found");
     },
   },
 };
