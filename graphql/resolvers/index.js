2961d24f72410f1cc1170cca0a1ba869c641b833
azkarmoulana <azkar.moulana@gmail.com>
2021-03-29 20:47:10 +0530

    added modifier for Post type

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -2,11 +2,18 @@ const postResolvers = require("./posts");
 const userResolvers = require("./users");
 
 module.exports = {
+  Post: {
+    likesCount: (parent) => parent.likes.length,
+    commentsCount: (parent) => parent.comments.length,
+  },
   Query: {
     ...postResolvers.Query,
   },
   Mutation: {
     ...userResolvers.Mutation,
     ...postResolvers.Mutation,
+  },
+  Subscription: {
+    ...postResolvers.Subscription,
   },
 };
